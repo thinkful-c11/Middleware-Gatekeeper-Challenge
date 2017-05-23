c82211c49d9076acf1e7c0d93590f595a1d1225a
Tanner Gill <contacttannergill@gmail.com>
2017-05-23 12:15:09 -0700

    Add files via upload

---
+++ app/gateKeeper.js
@@ -0,0 +1,49 @@
+'use strict';
+const queryString = require('query-string');
+
+const USERS = [
+  {id: 1,
+   firstName: 'Joe',
+   lastName: 'Schmoe',
+   userName: 'joeschmoe@business.com',
+   position: 'Sr. Engineer',
+   isAdmin: true,
+   // NEVER EVER EVER store passwords in plain text in real life. NEVER!!!!!!!!!!!
+   password: 'password'
+  },
+  {id: 2,
+   firstName: 'Sally',
+   lastName: 'Student',
+   userName: 'sallystudent@business.com',
+   position: 'Jr. Engineer',
+   isAdmin: true,
+   // NEVER EVER EVER store passwords in plain text in real life. NEVER!!!!!!!!!!!
+   password: 'password'
+  },
+  {id: 3,
+   firstName: 'Lila',
+   lastName: 'LeMonde',
+   userName: 'lila@business.com',
+   position: 'Growth Hacker',
+   isAdmin: false,
+   // NEVER EVER EVER store passwords in plain text in real life. NEVER!!!!!!!!!!!
+   password: 'password'
+  },
+  {id: 4,
+   firstName: 'Freddy',
+   lastName: 'Fun',
+   userName: 'freddy@business.com',
+   position: 'Community Manager',
+   isAdmin: false,
+   // NEVER EVER EVER store passwords in plain text in real life. NEVER!!!!!!!!!!!
+   password: 'password'
+  }
+];
+
+function gateKeeper(req, res, next) {
+  const userInfo = queryString.parse(req.get('x-username-and-password'));
+  req.user = USERS.find((user)=>{return user.userName === userInfo.userName})
+  next();
+}
+
+module.exports = {gateKeeper};
--- app/server.js
+++ app/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const {gateKeeper} = require('./gateKeeper');
 // you'll need to use `queryString` in your `gateKeeper` middleware function
 const queryString = require('query-string');
 
@@ -16,44 +17,44 @@ const app = express();
 // ways to keep user passwords as secure as possible).
 // You can learn mroe about password hashing later
 // here: https://crackstation.net/hashing-security.htm
-const USERS = [
-  {id: 1,
-   firstName: 'Joe',
-   lastName: 'Schmoe',
-   userName: 'joeschmoe@business.com',
-   position: 'Sr. Engineer',
-   isAdmin: true,
-   // NEVER EVER EVER store passwords in plain text in real life. NEVER!!!!!!!!!!!
-   password: 'password'
-  },
-  {id: 2,
-   firstName: 'Sally',
-   lastName: 'Student',
-   userName: 'sallystudent@business.com',
-   position: 'Jr. Engineer',
-   isAdmin: true,
-   // NEVER EVER EVER store passwords in plain text in real life. NEVER!!!!!!!!!!!
-   password: 'password'
-  },
-  {id: 3,
-   firstName: 'Lila',
-   lastName: 'LeMonde',
-   userName: 'lila@business.com',
-   position: 'Growth Hacker',
-   isAdmin: false,
-   // NEVER EVER EVER store passwords in plain text in real life. NEVER!!!!!!!!!!!
-   password: 'password'
-  },
-  {id: 4,
-   firstName: 'Freddy',
-   lastName: 'Fun',
-   userName: 'freddy@business.com',
-   position: 'Community Manager',
-   isAdmin: false,
-   // NEVER EVER EVER store passwords in plain text in real life. NEVER!!!!!!!!!!!
-   password: 'password'
-  }
-];
+// const USERS = [
+//   {id: 1,
+//    firstName: 'Joe',
+//    lastName: 'Schmoe',
+//    userName: 'joeschmoe@business.com',
+//    position: 'Sr. Engineer',
+//    isAdmin: true,
+//    // NEVER EVER EVER store passwords in plain text in real life. NEVER!!!!!!!!!!!
+//    password: 'password'
+//   },
+//   {id: 2,
+//    firstName: 'Sally',
+//    lastName: 'Student',
+//    userName: 'sallystudent@business.com',
+//    position: 'Jr. Engineer',
+//    isAdmin: true,
+//    // NEVER EVER EVER store passwords in plain text in real life. NEVER!!!!!!!!!!!
+//    password: 'password'
+//   },
+//   {id: 3,
+//    firstName: 'Lila',
+//    lastName: 'LeMonde',
+//    userName: 'lila@business.com',
+//    position: 'Growth Hacker',
+//    isAdmin: false,
+//    // NEVER EVER EVER store passwords in plain text in real life. NEVER!!!!!!!!!!!
+//    password: 'password'
+//   },
+//   {id: 4,
+//    firstName: 'Freddy',
+//    lastName: 'Fun',
+//    userName: 'freddy@business.com',
+//    position: 'Community Manager',
+//    isAdmin: false,
+//    // NEVER EVER EVER store passwords in plain text in real life. NEVER!!!!!!!!!!!
+//    password: 'password'
+//   }
+// ];
 
 
 // write a `gateKeeper` middleware function that:
@@ -62,11 +63,11 @@ const USERS = [
 //  3. looks for a user object matching the sent username and password values
 //  4. if matching user found, add the user object to the request object
 //     (aka, `req.user = matchedUser`)
-function gateKeeper(req, res, next) {
-  const userInfo = queryString.parse(req.get('x-username-and-password'));
-  req.user = USERS.find((user)=>{return user.userName === userInfo.userName})
-  next();
-}
+// function gateKeeper(req, res, next) {
+//   const userInfo = queryString.parse(req.get('x-username-and-password'));
+//   req.user = USERS.find((user)=>{return user.userName === userInfo.userName})
+//   next();
+// }
 
 app.use(gateKeeper);
 
